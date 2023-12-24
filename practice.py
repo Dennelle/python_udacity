9f48f63b3c3a66085438ab94e1d3ea040624b0f7
Dennelle <dennelle.walton@gmail.com>
2023-12-24 18:03:12 -0500

    setup

---
+++ practice.py
@@ -0,0 +1,70 @@
+# # Calculate the average of my utility bills for the past three months.
+# avg = (23 + 32 + 64)/3
+# print(avg)
+
+# In this quiz you're going to do some calculations for a tiler. Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+
+# How many tiles are needed?
+# You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+
+# tiles = 17 * 6
+# print(tiles)
+
+# part_one = 9 * 7
+# print(part_one)
+
+# part_two = 5 * 7
+# print(part_two)
+
+# leftover_tiles = tiles/(part_one + part_two)
+# print(leftover_tiles)
+
+# carrot = 24
+# rabbits = 8
+
+# crs_per_rab = carrot/rabbits
+# print(crs_per_rab)
+
+# rabbits = 12
+
+# crs_per_rab = carrot/rabbits
+
+# ## The current volume of a water reservoir (in cubic metres)
+# reservoir_volume = 4.445e8
+# ## The amount of rainfall from a storm (in cubic metres)
+# rainfall = 5e6
+
+# ## decrease the rainfall variable by 10% to account for runoff
+# rainfall *= .9
+# print(f"This is the rainfall variable decreased by 10% {rainfall}")
+
+# ## add the rainfall variable to the reservoir_volume variable
+# reservoir_volume += rainfall
+# print(f"This the reservior volume {reservoir_volume}")
+# ## increase reservoir_volume by 5% to account for stormwater that flows
+# ## into the reservoir in the days following the storm
+# reservoir_volume *= 1.05
+# print(f"This is if the reservior volume increased by 5% {reservoir_volume}")
+# ## decrease reservoir_volume by 5% to account for evaporation
+# reservoir_volume *= 0.95
+# print(f"This is if the reservior volume decreased by 5% {reservoir_volume}")
+# ## subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# ## that's piped to arid regions.
+# reservoir_volume -= 2.5e5
+# print(f"This is if we subtract 2.5e5 cubic metres from rv to account for water that is piped to arid regions {reservoir_volume}")
+
+# sf_population, sf_area = 864816, 231.89
+# rio_population, rio_area = 6453682, 486.5
+
+# san_francisco_pop_density = sf_population/sf_area
+# rio_de_janeiro_pop_density = rio_population/rio_area
+
+# # Write code that gives a boolean value True if San Francisco is denser than Rio, and False otherwise
+# comparision_result = san_francisco_pop_density > rio_de_janeiro_pop_density
+# print(comparision_result)
+
+my_string = "this is a string"
+my_string2 = 'this is also a string'
+
+this_string = 'Simon\'s skateboard is in the garage'
+print(this_string)
