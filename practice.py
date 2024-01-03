9fd477cff99b219ba625302ebbfce898e2b80eee
Dennelle <dennelle.walton@gmail.com>
2024-01-03 17:41:47 -0500

    dictionaries

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+import time
 # # Calculate the average of my utility bills for the past three months.
 # avg = (23 + 32 + 64)/3
 # print(avg)
@@ -247,3 +248,36 @@
 # print(fruits)
 # fruits.pop()
 # print(fruits)
+
+set1 = set(range(1000))
+set2 = set(range(500, 1500))
+list1 = list(set1)
+list2 = list(set2)
+
+# start_time = time.time()
+# union_set = set1.union(set2)
+# print("Set Union Time:", time.time() - start_time)
+
+# start_time = time.time()
+# union_list = list(set(list1 + list2))
+# print("List Union Time:", time.time() - start_time)
+
+# start_time = time.time()
+# intersection_set = set1.intersection(set2)
+# print("Set Intersection Time:", time.time() - start_time)
+
+# start_time = time.time()
+# intersection_list = [x for x in list1 if x in set2]
+# print("List Intersection Time:", time.time() - start_time)
+
+# start_time = time.time()
+# difference_set = set1.difference(set2)
+# print("Set Difference Time:", time.time() - start_time)
+
+# start_time = time.time()
+# difference_list = [x for x in list1 if x not in set2]
+# print("List Difference Time:", time.time() - start_time)
+
+# A dictionary is a mutable data type that stores mappings of unique keys to values. Here's a dictionary that stores elements and their atomic numbers.
+
+elements = {'hydrogen':1, 'helium':2, 'carbon':3}
